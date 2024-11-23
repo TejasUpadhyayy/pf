b4bd32779acdc36d34f99bbdb50503f2235768f1
TejasUpadhyayy <upadhyaytejas46@gmail.com>
2024-11-23 18:50:45 +0530

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <main>
-      <h1 className='text-5x1 text-org'>Welcome</h1>
+      <h1 className="text-5xl text-orange-500 font-bold">Welcome</h1>
     </main>
   )
 }
